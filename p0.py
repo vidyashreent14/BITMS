ce7c9cbffbe55621edf2755c28cbe32ec1fbcfe9
vidyashreent14 <167503756+vidyashreent14@users.noreply.github.com>
2024-04-27 11:40:08 +0530

    Add files via upload

---
+++ p0.py
@@ -0,0 +1,20 @@
+banana=[10,'vid',2500.75,'z']
+print(banana)
+
+for i in banana:
+    print(i,end="-")
+print()
+
+pineapple=[25000.75,
+    
+    
+
+
+
+
+
+
+
+
+
+
---
+++ p4.py
@@ -0,0 +1,5 @@
+vidya=[10,20,30,40,50]
+sum=0
+for i in vidya:
+    sum+=i
+print(sum)
